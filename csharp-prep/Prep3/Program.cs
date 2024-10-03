733c7db1e6905911f36cf4afb4baaf280ca92f33
Geraldo-Soto-7 <gerardo.o.soto.granados@gmail.com>
2024-10-03 00:41:02 -0600

    finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // For Parts 1 and 2
+       //Console.WriteLine("What is the magic number?");
+       //int number = int.Parse(Console.ReadLine());
+
+        // For Parts 3
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+       int guess_num = -1;
+
+       while (guess_num != number)
+       {
+        Console.WriteLine("What is your guess?");
+        guess_num = int.Parse(Console.ReadLine());
+
+       if(guess_num > number)
+       {
+         Console.WriteLine(" Lower ");
+       }
+
+       else if(guess_num < number)
+       {
+         Console.WriteLine(" Higher ");
+       }
+
+       else 
+       {
+         Console.WriteLine("You guess it! ");
+       }
+
+       }
+
+
     }
 }
