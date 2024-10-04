b2ef81a0f6e0acf2f3753b386693201890f9308d
Geraldo-Soto-7 <gerardo.o.soto.granados@gmail.com>
2024-10-03 23:29:54 -0600

    finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+         List <int> numbers = new List<int> ();
+
+         int usNumber = -1;
+         while (usNumber != 0)
+         {
+            Console.WriteLine("Enter a number: ");
+            
+            string usResponse = Console.ReadLine();
+            usNumber = int.Parse(usResponse);
+            
+            
+            if (usNumber != 0)
+            {
+                numbers.Add(usNumber);
+            }
+         }
+
+       int sum = 0;
+       foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+         float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+         int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+
     }
 }
